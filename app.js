464075c370f828a0dbd9092bc147d6ec3681b3f6
fazza7777 <aungbarlaay7777@gmail.com>
2022-07-06 14:03:04 +0630

    single fileupload use express-fileupload package and use middleware

--- app.js
+++ app.js
@@ -1,15 +1,28 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 mongoose.connect(`mongodb://localhost:27017/${process.env.DB}`)
 
+app.use(fileUpload());
 app.use(express.json());
 
 const userRoute = require('./routes/user');
 const postRoute = require('./routes/post');
 
+const testMiddleware = async (req, res, next) => {
+    // console.log(req.files);
+    // console.log(req.files.photo.name);
+    let file = req.files.photo;
+    let photoName = new Date().valueOf() + '_' + file.name;
+    file.mv(`./uploads/${photoName}`)
+    req.photoName = photoName;
+    next();
+}
+app.post('/gallery',testMiddleware, (req, res, next) => {
+    res.json({ photoName: req.photoName });
+})
 app.use('/users', userRoute);
 app.use('/posts', postRoute);
 
