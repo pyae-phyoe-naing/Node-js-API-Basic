abd690e05eca64cd9e9a03d230d8c9522f9f6cc0
fazza7777 <aungbarlaay7777@gmail.com>
2022-07-06 13:29:08 +0630

    post crud operation success

--- controller/post.js
+++ controller/post.js
@@ -1,22 +1,44 @@
 const Helper = require('../utils/helper');
+const DB = require('../schema/post');
 
 const all = async (req, res, next) => {
-    res.json({ msg: 'all posts' });
+    const posts = await DB.find();
+    Helper.flashMsg(res, "Get All Posts", posts);
 }
-const add = async(req,res, next)=> {
-        res.json({ msg: 'add new posts',result:req.body });
+const add = async (req, res, next) => {
+    const newPost = req.body;
+    const result = await new DB(newPost).save();
+    Helper.flashMsg(res, "Success add new post", result);
 
 }
-const get = async(req,res, next)=> {
-    res.json({ msg: 'get posts',result:req.params.id });
+const get = async (req, res, next) => {
+    const post = await DB.findById(req.params.id);
+    if (post) {
+        Helper.flashMsg(res, "Get single post", post);
+    } else {
+        next(new Error('Not have post this ID'));
+    }
 
 }
-const patch = async(req,res, next)=> {
-       res.json({ msg: 'update posts',result:req.body });
-
+const patch = async (req, res, next) => {
+    const post = await DB.findById(req.params.id);
+    if (post) {
+        await DB.findByIdAndUpdate(post._id, req.body);
+        const result = await DB.findById(post._id);
+        Helper.flashMsg(res, "Success update  post", result);
+    } else {
+        next(new Error('Update fail , post not found this ID'));
+    }
 }
-const drop = async(req,res, next)=> {
-    res.json({ msg: 'delete posts',result:req.params.id });
+const drop = async (req, res, next) => {
+    const post = await DB.findById(req.params.id);
+    if (post) {
+        const result = await DB.findByIdAndDelete(post._id, req.body);
+        Helper.flashMsg(res, "Success delete  post", result);
+
+    } else {
+        next(new Error('Delete fail ,post not found this ID'));
+    }
 }
 module.exports = {
     all,
---
+++ schema/post.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const {
+    Schema
+} = mongoose;
+
+const PostSchema = new Schema({
+    user: {
+        type: Schema.Types.ObjectId,
+        required: true,
+        ref: 'user'
+    },
+    title: { type: String, required: true },
+    desc: { type: String, required: true },
+    create : {type:Date,default:Date.now }
+});
+const post = mongoose.model('post', PostSchema);
+module.exports = post;
